47d9f0b2542756e14e1916e5c3d96b3949d19f9f
surajverma2587 <surajverma2587@gmail.com>
2023-06-05 20:00:50 +0100

    complete styles banner

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { Banner } from "./components/Banner";
+import { CalculatorForm } from "./components/CalculatorForm";
+
 export const App = () => {
-  return <div className="container">App</div>;
+  return (
+    <div className="container">
+      <Banner />
+      <CalculatorForm />
+    </div>
+  );
 };
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+export const Banner = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="my-3">Take Home Salary Calculator</h1>
+      <hr />
+      <h2 className="fs-6">
+        Calculate your take home salary for the year 2021/2022 or 2022/2023
+      </h2>
+    </div>
+  );
+};
